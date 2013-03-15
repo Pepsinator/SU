ef84834beeda6609df4f9b56e9d75cca58d4e11c
Marcus Løken <lm.loken@gmail.com>
2013-03-15 14:12:26 +0100

    Alarm del 6 (lagt til noen flotte hjelpefunksjoner)

--- src/kontroller/AlarmKontroller.java
+++ src/kontroller/AlarmKontroller.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import bibliotek.Funksjon;
+
 import visning.AlarmVisning;
 import visning.AvtaleVisning;
 import visning.GeneriskVisning;
@@ -60,6 +62,7 @@ public class AlarmKontroller extends AbstraktKontroller {
 		}
 	}
 	
+	//Viser egenskapene til en valgt alarm
 	public void visValgtAlarm(Alarm alarm) throws SQLException, FileNotFoundException, IOException{
 		GeneriskVisning.printTopp();
 		if (alarm == null) {
@@ -81,12 +84,51 @@ public class AlarmKontroller extends AbstraktKontroller {
 		} while (true);
 	}
 
+	//Viser avtaler som ikke har en alarm
 	public void visAvtalerUtenAlarm() throws FileNotFoundException, SQLException, IOException{
 		String sql = "select avtale.id from avtale where avtale.id NOT IN" +
 				"(select avtale.id from avtale, alarm where avtale_id = avtale.id)";
 		ArrayList<Avtale> avt = AvtaleListe.medSql(sql);
-//		System.out.println(Avtaler);
 		GeneriskVisning.printTopp();
 		AvtaleVisning.visAvtaler(avt);
+		System.out.println("Velg id for å legge til alarm:");
+		String inn;
+		int avtaleId;
+		do {
+			inn = this.ventStdInn();
+			try {
+				avtaleId = Integer.parseInt(inn);
+			}
+			catch (NumberFormatException u) {
+				break;
+			}
+			lagNyAlarm(avtaleId, avt);
+			return;
+		} while (false);
+	}
+	
+	
+	
+	public void lagNyAlarm(int avtaleId, ArrayList<Avtale> avtList ) throws FileNotFoundException, SQLException, IOException{
+		Avtale avt = Avtale.medId(avtaleId);
+		//Sjekker om avtalen ikke finnes eller allerede har alarm
+		if(avt == null ){
+			System.out.println("Avtalen finnes ikke");
+		}else if(avtList.contains(avt)){
+			System.out.println("Avtalen har allerede alarm");
+		}else{
+			//Tar inn en tid, looper helt til en gyldig tid blir valgt
+			System.out.println("Skriv inn tid før avtale (tt:mm:ss): ");
+			do{
+			String inn = this.ventStdInn();
+			if(!Funksjon.sjekkTidsFormat(inn)) {
+				System.out.println("Feil tidsformat");
+				continue;
+			}
+			int sek = Funksjon.tidTilSek(inn);
+			
+			}while(false);
+		}
+		
 	}
 }
